2a9c36af67d727b08527cad91df6a3e0947437e6
viviane NYAKOU <vivianenyakou@gmail.com>
2021-07-05 14:38:24 +0000

    Topbar ok

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -45,67 +45,58 @@
 			<div class="top-bar">
 				<div class="content-topbar flex-sb-m h-full container">
 					<div class="left-top-bar">
-						Free shipping for standard order over $100
+						Bienvenue sur notre site
 					</div>
 
 					<div class="right-top-bar flex-w h-full">
 						<a href="#" class="flex-c-m trans-04 p-lr-25">
-							Help & FAQs
+							FAQs
 						</a>
 
 						<a href="#" class="flex-c-m trans-04 p-lr-25">
-							My Account
+							utilisateur
 						</a>
 
 						<a href="#" class="flex-c-m trans-04 p-lr-25">
-							EN
+						     Contact
 						</a>
 
 						<a href="#" class="flex-c-m trans-04 p-lr-25">
-							USD
+							Qui sommes nous ?
 						</a>
 					</div>
 				</div>
 			</div>
 
 			<div class="wrap-menu-desktop">
-				<nav class="limiter-menu-desktop container">
-					
+				<nav class="limiter-menu-desktop container">				
 					<!-- Logo desktop -->		
 					<a href="#" class="logo">
-						<img src="images/icons/logo-01.png" alt="IMG-LOGO">
+						<img src="images/icons/Logo.png" alt="IMG-LOGO">
 					</a>
 
 					<!-- Menu desktop -->
 					<div class="menu-desktop">
 						<ul class="main-menu">
+							<li>
+								<a href="#">Accueil</a>
+							</li>
+
 							<li class="active-menu">
-								<a href="index.html">Home</a>
+								<a href="#">Categories</a>
 								<ul class="sub-menu">
-									<li><a href="index.html">Homepage 1</a></li>
-									<li><a href="home-02.html">Homepage 2</a></li>
-									<li><a href="home-03.html">Homepage 3</a></li>
+									<li><a href="#">Hommes</a></li>
+									<li><a href="#">Femmes</a></li>
+									<li><a href="#">Enfants</a></li>
 								</ul>
+							</li>							
+
+							<li>
+								<a href="">Promotion</a>
 							</li>
 
 							<li>
-								<a href="product.html">Shop</a>
-							</li>
-
-							<li class="label1" data-label1="hot">
-								<a href="shoping-cart.html">Features</a>
-							</li>
-
-							<li>
-								<a href="blog.html">Blog</a>
-							</li>
-
-							<li>
-								<a href="about.html">About</a>
-							</li>
-
-							<li>
-								<a href="contact.html">Contact</a>
+								<a href="">Contact</a>
 							</li>
 						</ul>
 					</div>	
