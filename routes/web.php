a426d5ffc6da945f99ddcc3ff43e6e7d97822ccd
viviane NYAKOU <vivianenyakou@gmail.com>
2021-07-08 10:42:26 +0000

    login

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('login','loginController');
+Route::resource('register','RegisterController');
+Route::resource('Accueil','AccueilController');
+
