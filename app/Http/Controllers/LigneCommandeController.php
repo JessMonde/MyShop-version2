68d8e5afbc144e5696ac90f7652bbfc5442e6a85
viviane NYAKOU <vivianenyakou@gmail.com>
2021-07-14 13:07:00 +0000

    migrations

---
+++ app/Http/Controllers/LigneCommandeController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Model\Ligne_commande;
+use Illuminate\Http\Request;
+
+class LigneCommandeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Model\Ligne_commande  $ligne_commande
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Ligne_commande $ligne_commande)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Model\Ligne_commande  $ligne_commande
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Ligne_commande $ligne_commande)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Model\Ligne_commande  $ligne_commande
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Ligne_commande $ligne_commande)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Model\Ligne_commande  $ligne_commande
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Ligne_commande $ligne_commande)
+    {
+        //
+    }
+}
